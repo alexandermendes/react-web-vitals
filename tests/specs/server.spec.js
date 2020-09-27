e949e7b9106d940c98647e72f6b408396ce6aa94
alexandermendes <alexanderhmendes@gmail.com>
2020-09-28 00:19:27 +0100

    test: add simple test for ssr

---
+++ tests/specs/server.spec.js
@@ -0,0 +1,10 @@
+/**
+ * @jest-environment node
+ */
+import { useWebVitals } from '../../src';
+
+describe('Server', () => {
+  it('does not throw when loaded on the server', () => {
+    expect(() => useWebVitals()).not.toThrow();
+  });
+});
